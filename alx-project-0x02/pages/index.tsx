f27e4b2631c4c9c53a316e9de1e31c1e667871d6
Limitlex Technologies <kobbygazi@gmail.com>
2025-11-23 18:10:29 +0000

    Created directory

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the home page. Enjoy exploring!</p>
+    </div>
+  );
+};
+
+export default Home;
