1d072c656dd751838d6e4a6ca1525e5128cb09dc
Limitlex Technologies <kobbygazi@gmail.com>
2025-11-24 22:17:19 +0000

    Updated index.tsx

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,21 +1,5 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  isActive: boolean;
+export interface CardProps {
+  title: string;
+  content: string;
 }
 
-export interface Product {
-  id: number;
-  name: string;
-  price: number;
-  inStock: boolean;
-}
-
-export interface Order {
-  id: number;
-  userId: number;
-  productIds: number[];
-  orderDate: string;
-  status: 'pending' | 'completed' | 'cancelled';
-}
